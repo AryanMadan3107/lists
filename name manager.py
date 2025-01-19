05e112e45c6014685d5051006198d6cff0e3d687
AryanMadan3107 <aryanmadan@outlook.com>
2025-01-19 12:00:01 +0100

    Add files via upload

--- name manager.py
+++ name manager.py
@@ -10,23 +10,25 @@ def menu():
 
 names = ["Anjali", "Aryan", "Immanuel"]
 
-c=int(input("Choose from the numbers 1-5 "))
-if c==1:
-    print(names)
-elif c==2:
-    a=input("Which name do you want to add? ")
-    names.append(a)
-elif c==3:
-    r=input("Which name would you like to remove? ")
-    names.remove(r)
-elif c==4:
-    ch=input("Which name would you like to change? ")
-    upd=input("To what would you like to change the name? ")
-    ind=0
-    for i in names:
-        if i==ch:
-            names[ind]=upd
-        else:
-            ind+=1
-elif c==5:
-    sys.exit()
+while True:
+    menu()
+    c=int(input("Choose from the numbers 1-5 "))
+    if c==1:
+        print(names)
+    elif c==2:
+        a=input("Which name do you want to add? ")
+        names.append(a)
+    elif c==3:
+        r=input("Which name would you like to remove? ")
+        names.remove(r)
+    elif c==4:
+        ch=input("Which name would you like to change? ")
+        upd=input("To what would you like to change the name? ")
+        ind=0
+        for i in names:
+            if i==ch:
+                names[ind]=upd
+            else:
+                ind+=1
+    elif c==5:
+        sys.exit()
